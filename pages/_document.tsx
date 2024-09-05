4093c1c230695d35238d8bf46661864367b764c8
Programmovic <adamforbusiness@outlook.com>
2024-09-05 19:55:11 +0300

    update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,19 +10,19 @@ export default class Document extends NextDocument {
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/static/favicons/apple-touch-icon.png"
+            href="https://auto-stock-market.vercel.app/images/logos/logo-asm.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/static/favicons/favicon-32x32.png"
+            href="https://auto-stock-market.vercel.app/images/logos/logo-asm.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/static/favicons/favicon-16x16.png"
+            href="https://auto-stock-market.vercel.app/images/logos/logo-asm.png"
           />
           <link rel="manifest" href="/static/favicons/manifest.json" />
         </Head>
